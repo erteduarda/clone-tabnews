51afe19adb745a3bc3c87b3315d0b1c05a4fd04a
Eduarda Tavares <erteduarda@gmail.com>
2025-12-29 22:42:24 -0300

    finish `/api/v1/status` endpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
 test("GET to /api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
+  const response = await fetch("http://localhost:3001/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
+
+  expect(responseBody.dependences.database.version).toEqual("16.0");
+  expect(responseBody.dependences.database.max_connections).toEqual(100);
+  expect(responseBody.dependences.database.open_connections).toEqual(1);
 });
